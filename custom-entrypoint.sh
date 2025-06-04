7cf4c8299770faa27f5f4879dc58dedf187c5ec0
SleepyHead <53241974+techroy23@users.noreply.github.com>
2025-06-05 06:02:22 +0800

    Update and rename entrypoint.sh to custom-entrypoint.sh

---
+++ custom-entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+echo "Executing custom entrypoint ..."
+
+setup_peer2profit() {
+  CONFIG_FILE="/root/.config/org.peer2profit.peer2profit.ini"
+  FLAG_FILE="/root/.config/org.peer2profit.setup_done"
+
+  if [ -z "$P2P_EMAIL" ]; then
+    echo "P2P_EMAIL is not set or is blank. Skipping creation of the Peer2Profit configuration file."
+  fi
+
+  if [ -f "$FLAG_FILE" ]; then
+    echo "Configuration already exists. Skipping."
+    return 0
+  fi
+
+  mkdir -p "$(dirname "$CONFIG_FILE")"
+
+  cat <<EOF > "$CONFIG_FILE"
+[General]
+Username=$P2P_EMAIL
+hideToTrayMsg=false
+installid2=$(uuidgen)
+locale=en_US
+EOF
+
+  chown -R "root:root" "$(dirname "$CONFIG_FILE")"
+  touch "$FLAG_FILE"
+
+  echo "Configuration created successfully at $CONFIG_FILE."
+}
+
+echo " Starting Peer2Profit ..."
+setup_peer2profit
+/usr/bin/peer2profit &
